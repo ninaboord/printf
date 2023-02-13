592a2f4beb9b977f551580c7ef2db68800f16ba1
Nina Boord <86579493+ninaboord@users.noreply.github.com>
2023-02-13 12:38:42 -0800

    Update printf.c

--- printf.c
+++ printf.c
@@ -4,10 +4,6 @@
 #include "strings.h"
 #include "uart.h"
 
-/** Prototypes for internal helpers.
- * Typically these would be qualified as static (private to module)
- * but, in order to call them from the test program, we declare them externally
- */
 int unsigned_to_base(char *buf, 
                      size_t bufsize, 
                      unsigned int val, 
@@ -218,38 +214,3 @@ int printf(const char *format, ...)
     return res;
 }
 
-
-/* From here to end of file is some sample code and suggested approach
- * for those of you doing the disassemble extension. Otherwise, ignore!
- *
- * The struct insn bitfield is declared using exact same layout as bits are organized in
- * the encoded instruction. Accessing struct.field will extract just the bits
- * apportioned to that field. If you look at the assembly the compiler generates
- * to access a bitfield, you will see it simply masks/shifts for you. Neat!
-
-
-static const char *cond[16] = {"eq", "ne", "cs", "cc", "mi", "pl", "vs", "vc",
-                               "hi", "ls", "ge", "lt", "gt", "le", "", ""};
-static const char *opcodes[16] = {"and", "eor", "sub", "rsb", "add", "adc", "sbc", "rsc",
-                                  "tst", "teq", "cmp", "cmn", "orr", "mov", "bic", "mvn"};
-
-struct insn  {
-    uint32_t reg_op2:4;
-    uint32_t one:1;
-    uint32_t shift_op: 2;
-    uint32_t shift: 5;
-    uint32_t reg_dst:4;
-    uint32_t reg_op1:4;
-    uint32_t s:1;
-    uint32_t opcode:4;
-    uint32_t imm:1;
-    uint32_t kind:2;
-    uint32_t cond:4;
-};
-
-static void sample_use(unsigned int *addr) {
-    struct insn in = *(struct insn *)addr;
-    printf("opcode is %s, s is %d, reg_dst is r%d\n", opcodes[in.opcode], in.s, in.reg_dst);
-}
-
-*/
